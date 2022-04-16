fef33f3b505a62d340c5f7fc95e061d3814b585e
JerryLo <mj1994tw@gmai.com>
2022-04-16 14:23:03 +0800

    Initial Commit

---
+++ AnimeAndManga/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  AnimeAndManga
+//
+//  Created by JerryLo on 2022/4/16.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
